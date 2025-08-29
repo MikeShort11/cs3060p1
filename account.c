d342cc784a3eccd97fc5fee994e4a554343b5370
michael <michael.short094@gmail.com>
2025-08-28 23:36:27 -0600

    starting project

---
+++ account.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <string.h>
+
+#include "account.h"
+
+int account_init(Account *acct_p, int id, int balance, char *name) {
+
+}
---
+++ main2.c
@@ -0,0 +1,4 @@
+#include <stdio.h>
+#include "account.h"
+
+
